08cb8c22e3491e0a33d652db72a43ae5cb405a56
Кирилл Трачук <>
2021-06-22 12:35:32 +0300

    adaptive fix

--- app/js/app.js
+++ app/js/app.js
@@ -19,25 +19,34 @@ document.addEventListener('DOMContentLoaded', () => {
 		let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 		let per = windowScroll / windowHeight * 100;
 		progress.style.width = per + '%'
+		console.log(windowHeight)
 	}
 
-	gsap.to(".preloader", 2.2, {
-		delay: 5,
+	gsap.to(".preloader", 3.2, {
+		delay: 6,
 		top: "-3000px",
 		ease: Expo.easeInOut
 	})
 
 
-	$('.slider-for').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		fade: false,
-		draggable: true,
-		vertical: true,
-		infinite: false,
-		verticalSwiping: true,
-		adaptiveHeight: true,
-	});
+	// $('.slider-for').slick({
+	// 	slidesToShow: 1,
+	// 	slidesToScroll: 1,
+	// 	arrows: false,
+	// 	fade: false,
+	// 	draggable: true,
+	// 	vertical: true,
+	// 	infinite: false,
+	// 	verticalSwiping: true,
+	// 	adaptiveHeight: true,
+	// });
+
+	let mobileButton = document.querySelector('.bt-menu')
+	let mobileMenu = document.querySelector('.mobile_menu')
+
+	mobileButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active')
+    mobileButton.classList.toggle('active')
+ })
 
 })
